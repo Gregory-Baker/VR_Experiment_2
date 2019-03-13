c9adbd50571812d3f68b075135797186fc6adf69
Gregory-Baker <gregory.baker@brl.ac.uk>
2019-03-13 11:21:52 +0000

    Added path and navmesh

---
+++ Assets/SampleAgentScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SampleAgentScript : MonoBehaviour
+{
+    public Transform target;
+    NavMeshAgent agent;
+    LineRenderer line;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        line = this.GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+        OnDrawGizmosSelected();
+    }
+
+    void OnDrawGizmosSelected()
+    {
+
+        if (agent == null || agent.path == null)
+            return;
+
+        if (line == null)
+        {
+            line.startWidth = 0.5f;
+            line.endWidth = 0.5f;
+            line.startColor = Color.yellow;
+        }
+
+        var path = agent.path;
+
+        line.positionCount = path.corners.Length;
+
+        for (int i = 0; i < path.corners.Length; i++)
+        {
+            line.SetPosition(i, path.corners[i]);
+        }
+
+    }
+}
