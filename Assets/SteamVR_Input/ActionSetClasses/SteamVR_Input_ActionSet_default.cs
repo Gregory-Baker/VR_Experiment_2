286c61f0610b6545c629dbaa4c157039d10dabad
Gregory-Baker <gregory.baker@brl.ac.uk>
2019-03-15 11:37:02 +0000

    Implemented stop robot action on controller grip

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -89,6 +89,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean StopRobot
+        {
+            get
+            {
+                return SteamVR_Actions.default_StopRobot;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
