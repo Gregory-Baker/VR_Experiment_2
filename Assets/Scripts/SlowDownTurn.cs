31618e61156a446c1eed0595bc33d053de1042ab
Gregory-Baker <gregory.baker@brl.ac.uk>
2019-03-18 14:09:58 +0000

    About to dechild 'player' from 'robot' to decopule rotation

---
+++ Assets/Scripts/SlowDownTurn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowDownTurn : MonoBehaviour
+{
+    public float maxTurnAnglePerFrame = 1f;
+
+    int frame = 0;
+
+    public float previousHeadAngle;
+    public float currentHeadAngle;
+    public float headAngleDiff;
+
+    float angleDiff;
+
+    void Start()
+    {
+        previousHeadAngle = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentHeadAngle = transform.eulerAngles.y;
+        angleDiff = currentHeadAngle - previousHeadAngle;
+        if (angleDiff > 180)
+        {
+            angleDiff -= 360;
+        }
+        else if (angleDiff < -180)
+        {
+            angleDiff += 360;
+        }
+        headAngleDiff = Mathf.Abs(angleDiff);
+
+        if (headAngleDiff > maxTurnAnglePerFrame)
+        {
+            print(headAngleDiff);
+        }
+        // previousTransform = transform;
+
+        frame++;
+        previousHeadAngle = transform.eulerAngles.y;
+    }
+}
