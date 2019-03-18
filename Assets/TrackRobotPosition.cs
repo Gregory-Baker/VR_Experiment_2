1df257cb7d568eecf2d0d49c8f68b0831473306b
Gregory-Baker <gregory.baker@brl.ac.uk>
2019-03-18 18:00:35 +0000

    About to change robot tracking to world

--- Assets/TrackRobotPosition.cs
+++ Assets/TrackRobotPosition.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,23 +7,35 @@ public class TrackRobotPosition : MonoBehaviour
 {
     public GameObject robot;
 
-    public bool egocentric = true;
+    public Camera vrCamera;
 
-    public int frame = 0;
+    public bool egocentric = true;
+    public bool trackRotation = false;
+
+    public float maxTurnAnglePerSecond = 10f;
 
     float currentX;
     float currentZ;
-
     float previousX;
     float previousZ;
 
     Vector3 playerTranslation;
+
+    float currentHeadAngle;
+    float currentRobotAngle;
+    float previousRobotAngle;
+    float angleDiff;
+    public float headRotationAngle;
+
+    Vector3 playerRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         previousX = robot.transform.position.x;
         previousZ = robot.transform.position.z;
+        vrCamera = GetComponentInChildren<Camera>();
+        previousRobotAngle = robot.transform.eulerAngles.y;
     }
 
     // Update is called once per frame
@@ -31,17 +44,52 @@ public class TrackRobotPosition : MonoBehaviour
 
         if (egocentric)
         {
-            currentX = robot.transform.position.x;
-            currentZ = robot.transform.position.z;
+            TranslatePlayerWithRobot();
 
-            playerTranslation.x = currentX - previousX;
-            playerTranslation.z = currentZ - previousZ;
-
-            transform.Translate(playerTranslation);
-
-            previousX = currentX;
-            previousZ = currentZ;
+            if (trackRotation)
+            {
+                TurnPlayerWithRobot();
+            }
         }
-        frame++;
+    }
+
+    private void TranslatePlayerWithRobot()
+    {
+        currentX = robot.transform.position.x;
+        currentZ = robot.transform.position.z;
+
+        playerTranslation.x = currentX - previousX;
+        playerTranslation.z = currentZ - previousZ;
+
+        transform.Translate(playerTranslation);
+
+        previousX = currentX;
+        previousZ = currentZ;
+    }
+
+    private void TurnPlayerWithRobot()
+    {
+        currentRobotAngle = robot.transform.eulerAngles.y;
+        // currentHeadAngle = vrCamera.transform.eulerAngles.y;
+        // angleDiff = currentRobotAngle - currentHeadAngle;
+        angleDiff = currentRobotAngle - previousRobotAngle;
+
+        if (angleDiff > 180)
+        {
+            angleDiff -= 360;
+        }
+        else if (angleDiff < -180)
+        {
+            angleDiff += 360;
+        }
+        angleDiff = Mathf.Abs(angleDiff);
+
+        headRotationAngle = Mathf.Min(angleDiff, 1000); // maxTurnAnglePerSecond * Time.deltaTime);
+
+        playerRotation.y = headRotationAngle;
+
+        transform.Rotate(transform.up, headRotationAngle);
+
+        previousRobotAngle = currentRobotAngle;
     }
 }
