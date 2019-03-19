9d76222f6c8af1dbf12eb3a67d972d956fbe0a1d
Gregory-Baker <gregory.baker@brl.ac.uk>
2019-03-19 16:54:33 +0000

    Removed failed jerk-reduction code

--- Assets/TrackRobotPosition.cs
+++ Assets/TrackRobotPosition.cs
@@ -11,9 +11,6 @@ public class TrackRobotPosition : MonoBehaviour
     public bool trackRotation = false;
 
     public float maxTurnAnglePerSecond = 10f;
-    public float maxHeadRotationAcc = 100f;
-
-    int frame;
 
     float currentX;
     float currentZ;
@@ -24,14 +21,9 @@ public class TrackRobotPosition : MonoBehaviour
 
     float currentPlayerAngle;
     float currentRobotAngle;
-    float previousHeadRotationAngle = 0;
     float angleDiff;
-    public float headRotationAngle;
-    public float headRotationVel;
-    float previousHeadRotationVel = 0;
-    public float headRotationAcc;
+    float headRotationAngle;
 
-    Vector3 playerRotation;
 
     // Start is called before the first frame update
     void Start()
@@ -43,9 +35,6 @@ public class TrackRobotPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        frame++;
-
         if (egocentric)
         {
             TranslatePlayerWithRobot();
@@ -95,49 +84,6 @@ public class TrackRobotPosition : MonoBehaviour
             headRotationAngle = Mathf.Sign(angleDiff) * maxTurnAnglePerSecond * Time.deltaTime;
         }
 
-        headRotationVel = (headRotationAngle - previousHeadRotationAngle) / Time.deltaTime;
-        headRotationAcc = (headRotationVel - previousHeadRotationVel) / Time.deltaTime;
-
-        //if (Mathf.Abs(headRotationAcc) > maxHeadRotationAcc)
-        //{
-        //    print("Original Angle: " + headRotationAngle);
-        //    for (int i = 0; i < 5; i++)
-        //    {
-        //        if (Mathf.Abs(headRotationAcc) < maxHeadRotationAcc)
-        //        {
-        //            print("Final Angle: " + headRotationAngle);
-        //            break;
-        //        }
-        //        else
-        //        {
-        //            headRotationAngle = headRotationAngle / 2;
-        //            headRotationVel = (headRotationAngle - previousHeadRotationAngle) / Time.deltaTime;
-        //            headRotationAcc = (headRotationVel - previousHeadRotationVel) / Time.deltaTime;
-        //        }
-        //    }
-        //    print("Final Angle2: " + headRotationAngle);
-        //}
-
-        //if (Mathf.Abs(headRotationAcc) > maxHeadRotationAcc && frame % 3 == 0)
-        //{
-        //    headRotationVel = Mathf.Sign(angleDiff) * maxHeadRotationAcc * Time.deltaTime + previousHeadRotationVel;
-        //    headRotationAngle = headRotationVel * Time.deltaTime + previousHeadRotationAngle;
-        //    print("Acceleration: " + headRotationAcc);
-        //}
-
-        //while (Mathf.Abs(headRotationAcc) > maxHeadRotationAcc)
-        //{
-        //    headRotationAngle *= 0.5f;
-        //    headRotationVel = (headRotationAngle - previousHeadRotationAngle) / Time.deltaTime;
-        //    headRotationAcc = (headRotationVel - previousHeadRotationVel) / Time.deltaTime;
-        //    print(headRotationAcc);
-        //}
-
         transform.Rotate(transform.up, headRotationAngle);
-
-
-        previousHeadRotationAngle = headRotationAngle;
-        previousHeadRotationVel = headRotationVel;
-
     }
 }
